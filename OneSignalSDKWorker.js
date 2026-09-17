913e26e865a9d2b3ad6da184a3d203eee0860e0a
PasoEnJuarez <pasoenjuarez@gmail.com>
2026-09-17 03:59:47 -0600

    Add message event listener to OneSignalSDKWorker
    Added an event listener to silence Chrome warnings.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+self.addEventListener('message', (event) => {
+  // Oyente inicial para silenciar la advertencia de Chrome
+});
